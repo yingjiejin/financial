93bde80e5701b7484b71d207d642690c67b0632f
jinyingjie <jinyingjie@jinyingjie.local>
2018-08-18 14:12:32 +0800

    swagger 优化

--- manager/src/main/java/com/jyj/manager/ManagerApp.java
+++ manager/src/main/java/com/jyj/manager/ManagerApp.java
@@ -1,5 +1,6 @@
 package com.jyj.manager;
 
+import com.jyj.swagger.EnableMySwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
